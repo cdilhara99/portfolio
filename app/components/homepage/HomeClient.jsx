79a7bbdbc82180c0b4f89d72cf12addea881ef6d
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-17 18:00:45 +0530

    UPD

---
+++ app/components/homepage/HomeClient.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import HeroSection from "../hero-section";
+import AboutSection from "../about";
+import Experience from "../experience";
+import Skills from "../skills";
+import Projects from "../projects";
+import Education from "../education";
+import ContactSection from "../contact";
+
+export default function HomeClient() {
+  return (
+    <div suppressHydrationWarning>
+      <HeroSection />
+      <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      {/* <Blog blogs={blogs} /> */}
+      <ContactSection />
+    </div>
+  );
+}
