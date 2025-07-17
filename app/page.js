af3765410f5e4f2a31468c59ca7df94e7732db2e
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-17 17:18:53 +0530

    UP

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import dynamic from 'next/dynamic'
+ 
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('./components/homepage/blog'),
+  { ssr: false }
+)
 
 // async function getData() {
 //   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
