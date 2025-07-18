a064b68ffc7e3ff207a8d1cdc973dc92da892b30
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-18 09:41:48 +0530

    hj

---
+++ app/components/LayoutClient.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import ScrollToTop from "../helper/scroll-to-top";
+
+export default function LayoutClient() {
+  return (
+    <>
+      <ToastContainer />
+      <ScrollToTop />
+    </>
+  );
+}
