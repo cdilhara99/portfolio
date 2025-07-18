b1d11ed017957ba600326de12eb1edba513bc1fd
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-18 10:06:25 +0530

    PP

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* <LayoutClient /> */}
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
+          {/* <Navbar /> */}
           {children}
         </main>
         {/* <Footer /> */}
