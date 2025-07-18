5eb237ac9a1c6e82d540c5ce3dad35394ba4d386
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-18 13:10:17 +0530

    LTS7-18-1-10

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -18,13 +18,12 @@ const Projects = () => {
 
       <div className="pt-24">
         <div className="flex flex-col gap-6">
-          {projectsData.slice(0, 8).map((project, index) => (
+          {projectsData.map((project, index) => (
             <div
-              id={`sticky-card-${index + 1}`}
               key={index}
-              className="sticky-card w-full mx-auto max-w-2xl sticky"
+              className="w-full mx-auto max-w-2xl sticky top-24 z-10"
             >
-              <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
+              <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s] bg-[#0d1224]">
                 <ProjectCard project={project} />
               </div>
             </div>
