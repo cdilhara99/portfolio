43c7849490e5209134318b5be5ad93131fcacf6e
Chamod Dilhara <Chamod.Dilhara@hirdaramani.com>
2025-07-18 10:42:44 +0530

    AA

--- app/components/homepage/HomeClient.jsx
+++ app/components/homepage/HomeClient.jsx
@@ -18,7 +18,7 @@ export default function HomeClient() {
       {/* <Projects /> */}
       {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
-      <ContactSection />
+      {/* <ContactSection /> */}
     </div>
   );
 }
